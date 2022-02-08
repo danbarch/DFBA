f0ab06b4fab046394a11c2c43998babb1c0d680a
Barch, Daniel <dbarch@rti.org>
2022-02-07 19:19:20 -0500

    Update MW and Wilcoxon 2-7-22

--- Development/dfba_wilcoxon.R
+++ Development/dfba_wilcoxon.R
@@ -1,219 +1,303 @@
+dfba_mann_whitney<-function(E,C,prior_vec=c(1,1),prob_interval=.95,samples=30000,method=""){
+  if (length(prior_vec)!=2){
+    stop("an explicit stipulation of prior_vec must only have the two shape parameters for the prior beta distribution")} else {}
 
-dfba_wilcoxon<-
-  function(Y1,Y2,method,priorvalues=rep(1/200,200)){
-    Y1mean=mean(Y1,na.rm=TRUE)
-    Y2mean=mean(Y2,na.rm=TRUE)
-    cat("Y1 mean","   ","Y2 mean","\n")
-    cat(Y1mean,"  ",Y2mean,"\n")
-    l1=length(Y1)
-    l2=length(Y2)
-    if (l1!=l2) {
-      stop("Y1 and Y2 must have the same length. This function is within-block data with no missing values.")} else {}
+  a0<-prior_vec[1]
+  b0<-prior_vec[2]
 
-    dt=Y1-Y2
-    d=Y1
-    jc=0
-    for (j in 1:l1){
-      if (is.na(dt[j])){} else {
-        jc=jc+1
-        d[jc]=dt[j]}
-    }
-    d=d[1:jc]
-    l1=jc
+  if ((a0<=0)|(b0<=0)){
+    stop("Both of the beta shape parameters for in the prior_vec must be >0.")} else {}
 
-    if (l1<3){
-      stop("There are not enough values in the Y1 and Y2 vector for meaningful results.")} else {}
+  if ((prob_interval<0)|(prob_interval>1)){
+    stop("The probability for the interval estimate of phi_w must be a proper proportion.")}
+  else {}
 
-    #The following code computes the within-block difference scores, and finds the
-    #number of blocks where the difference scores are nonzero (within a trivial rounding error).
-    sdd=sd(d)
-    IC=0
-    for (I in 1:l1){
-      if (abs(d[I])<=sdd/30000){IC=IC} else {IC=IC+1}}
-    n=IC
-    #The following code deals with the case where all differnces are trivially close to 0.
-    if (n==0){stop("Y1 and Y2 differences are all trivial")} else {}
+  if (samples<10000){
+    stop("stipulating Monte Carlo samples < 10000 is too small")} else {}
 
-    #The following finds the Tplus and Tminus stats and the number of nonzero blocks
-    dt=(seq(1,n,1))*0
-    IC=0
-    for (I in 1:l1){
-      if (abs(d[I])<=sdd/30000){IC=IC} else {
-        IC=IC+1
-        dt[IC]=d[I]}}
-    dta=(seq(1,n,1))*0
-    for (I in 1:n){
-      dta[I]=abs(dt[I])}
-    #The vector dtar is for the ranks of the absolute-value d scores; whereas
-    #the dta vector is for the absolute-value of the d scores, and dtars is
-    #the vector of signed rank scores.
-    dtar=rank(dta)
-    dtars=dtar*dt/dta
-    #The following computes the Tplus and Tminus statistics
-    tplus=0
-    for (I in 1:n){
-      if (dtars[I]>0){tplus=tplus+dtar[I]} else {tplus=tplus} }
-    tneg=(n*(n+1)*.5)-tplus
-    cat("n","  ","T_plus","  ","T_negative","\n")
-    #desstat<-c(n,tplus,tneg)
-    #print(desstat)
-    cat(n,"  ",tplus,"      ",tneg,"\n")
-
-    if (method=="small"){
-      m1lable<-"Following is based on 30000 Monte Carlo samples"
-      m2lable<-" with discrete prob. values."
-      cat(m1lable,m2lable,"\n")
-      if (length(priorvalues)!=200) {
-        stop("priorvalues must have a length of 200 or use the default by omitting priorvalues")}
-      else {}
-      totprior=sum(priorvalues)
-      if ((totprior>1.01)|(totprior<.99)){
-        priorvalues=rep(1/200,200)
-        message("User's prior did not sum to 1, so a flat prior was used instead.")} else {}
-
-
-      samples=30000
-      priorvector=priorvalues
-      fphi<-rep(0.0,200)
-      for (j in 1:200){
-        phi=1/(400)+(j-1)*(1/200)
-        for (k in 1:samples){
-          tz=sum((1:n)*rbinom(n, 1, phi))
-
-          if(tz==tplus) {
-            fphi[j]=fphi[j]+1.0
-          } else{
-            fphi[j]=fphi[j]
-          }
-        }
-      }
-      #The tot value below is the denominator of the discrete analysis,
-      #phipost is the vector for the posterior distribution.
-      tot=sum(priorvector*fphi)
-      phipost=(priorvector*fphi)/tot
-      phiv=rep(0.0,200)
-      phibar=0.0
-      for (j in 1:200){
-        phiv[j]=(1/400)+(j-1)*(1/200)
-        phibar=phibar+(phiv[j]*phipost[j])}
-      print("mean for phi_w is:")
-      print(phibar)
-      postdis<-data.frame(phiv,phipost)
-
-      plot(phiv,phipost,type="l",xlab="phi_w",ylab="posterior discrete probabilities",main="Based on Monte Carlo Samples")
-      #The following finds the poserior cumulative distribution and outputs these values.
-      cum_phi=cumsum(phipost)
-      phi_w=phiv
-      cumdis<-data.frame(phi_w,cum_phi)
-      #The prH1 is the probability that phi_w is greater than .5.
-      prH1=1-cum_phi[round(100)]
-      print("probability that phi_w exceeds .5 is:")
-      print(prH1)
-      #Following finds the Bayes factor for phi_w being greater than .5.
-      cum_prior=cumsum(priorvector)
-      priorprH1=1-cum_prior[round(100)]
-      if ((prH1==1)|(priorprH1==0)){
-        BF10=2*samples
-        print("Bayes factor BF10 for phi_w >.5 is estimated to be greater than:")
-        print(BF10)} else {
-          BF10=(prH1*(1-priorprH1))/(priorprH1*(1-prH1))
-          print("Bayes factor BF10 for phi_w>.5 is:")
-          print(BF10)}
-      #Following provides output of key statistics.
-      #list(posterior_cum_distribution=cumdis)
-      #m1X=" "
-      #m2X=" "
-      return(list(posterior_discrete_values=phipost,posterior_cum_distribution=cumdis))}
-
-    if (method=="large"){
-      m1L<-"Following is based on beta approximation for phi_w"
-      m2L<-"which is highly accurate for n>24"
-      cat(m1L,m2L,"\n")
-      cat(" ","   ","\n")
-      if (length(priorvalues)!=2) {
-        stop("priorvalues must have two value which are a0 and b0")}
-      else {}
-      if ((priorvalues[1]<=0)|(priorvalues[2]<=0)){
-        stop("both of the shape parameters for priorvalues must be >0. Use 1 for each for a flat prior.")} else {}
-
-      #The following code finds the shape parameters of a beta
-      #distribution that approximates the posterior distribution
-      #for the phi_w parameter
-      na0=priorvalues[1]-1
-      nb0=priorvalues[2]-1
-      term=(3*tplus)/((2*n)+2)
-      na=term+.25
-      nb=(((3*n)-1)/4)-term
-      a=na+na0+1
-      b=nb+nb0+1
-      x=seq(0,1,.005)
-      y=dbeta(x,a,b)
-      y0=dbeta(x,priorvalues[1],priorvalues[2])
-      plot(x,y,type="l",xlab="phi_w",ylab="probability density",main="posterior solid; prior dashed")
-      lines(x,y0,type="l",lty=2)
-
-      postmean=a/(a+b)
-      postmedian=qbeta(.5,a,b)
-      ms1="posterior shape parameters are"
-      ms2=":"
-      cat(ms1,ms2,"\n")
-      cat(a,"  ",b,"\n")
-      cat("  ","   ","\n")
-      mc1="posterior mean"
-      mc2="posterior median"
-      cat(mc1,"  ",mc2,"\n")
-      cat(postmean," ",postmedian,"\n")
-
-      qlequal=qbeta(.025,a,b)
-      qhequal=qbeta(.975,a,b)
-
-      met1="equal-tail 95-percent limit values are"
-      met2=":"
-      cat(met1,met2,"\n")
-      cat(qlequal," ",qhequal,"\n")
-
-      alphaL=seq(0,.05,.05/1000)
-      qL=qbeta(alphaL,a,b)
-      qH=qbeta(.95+alphaL,a,b)
-      diff=qH-qL
-      I=1
-      mindiff=min(diff)
-      while (diff[I]>mindiff){
-        I=I+1}
-      qLmin=qL[I]
-      qHmax=qH[I]
-      mhdi1="95-percent highest density limits are"
-      mhdi2=":"
-      cat(mhdi1,mhdi2,"\n")
-      cat(qLmin," ",qHmax,"\n")
-
-      prH1=1-pbeta(.5,a,b)
-      #print("Posterior probabilty that phi_w > .5 is:")
-      #print(prH1)
-      priorprH1=1-pbeta(.5,na0+1,nb0+1)
-      #print("Prior probabilty that phi_w > .5 is:")
-      #print(priorprH1)
-      mH11="assessing the probability that phi_w > .5"
-      mH12=" "
-      cat(mH11,mH12,"\n")
-      mH1prior="prior"
-      mH1post="posterior"
-      cat(mH1prior,"  ",mH1post,"\n")
-      cat(priorprH1,"  ",prH1,"\n")
-      if ((prH1==1)|(priorprH1==0)){
-        minf1="Bayes factor BF10 for phi_w >.5 is approaching"
-        minf2="infinity"
-        cat(minf1,minf2,"\n")} else {
-          BF10=(prH1*(1-priorprH1))/(priorprH1*(1-prH1))
-          mBF1="Bayes factor BF10 for phi_w > .5"
-          mBF2="is"
-          cat(mBF1,mBF2,"\n")
-          print(BF10)}
-      m1X=" "
-      m2X=" "
-      return(cat(m1X,m2X,"\n"))}
-
-    if ((method!="large")&(method!="small")) {
-      stop("method must be included, and it must equal either the word small or large")} else {}
+  Etemp=E
+  Ctemp=C
+  jc=0
+  for (j in 1:length(E)){
+    if (is.na(Etemp[j])){} else {
+      jc=jc+1
+      E[jc]=Etemp[j]}
   }
+  E=E[1:jc]
 
+  kc=0
+  for (k in 1:length(C)){
+    if (is.na(Ctemp[k])){} else {
+      kc=kc+1
+      C[kc]=Ctemp[k]}
+  }
+  C=C[1:kc]
+
+  nE=length(E)
+  nC=length(C)
+  if ((nE==0)|(nC==0)){
+    stop("The E and C vectors must have a length greater than 0.")} else {}
+  Emean=mean(E)
+  Cmean=mean(C)
+  cat("E mean","    ","C mean","\n")
+  cat(Emean," ",Cmean,"\n")
+  cat("n_E","  ","n_C","\n")
+  cat(nE,"   ",nC,"\n")
+  cat(" ","   ","\n")
+
+  #following code finds U_E and U_C
+  UE_vector<-rep(NA, length(E)) # UE counter
+  UC_vector<-rep(NA, length(C)) # UC counter
+  for (i in 1:length(E)){
+    UE_vector[i]<-sum(E[i]>C)
+  }
+  for (j in 1:length(C)){
+    UC_vector[j]<-sum(C[j]>E)
+  }
+  UE=sum(UE_vector)
+  UC=sum(UC_vector)
+  cat("U_E and U_C Mann-Whitney statistics are:"," ","\n")
+  cat("U_E","   ","U_C","\n")
+  cat(UE,"   ",UC,"\n")
+  cat(" ","   ","\n")
+  cat(" ","  ","\n")
+
+  nH=(2*nE*nC)/(nE+nC)
+
+  if (method==""){
+    if (nH>=20){method="large"} else {
+      method="small"}
+  } else {}
+
+  if (method=="small"){
+    m1lable<-"Following is based on Monte Carlo samples"
+    m2lable<-"with discrete prob. values."
+    cat(m1lable,m2lable,"\n")
+    cat("The number of Monte Carlo samples is:"," ","\n")
+    cat(samples," ","\n")
+    cat(" ","   ","\n")
+
+    #Code for the discrete prior
+    phiv=seq(1/400,.9975,.005)
+    x=phiv+.0025
+    priorvector=rep(0,200)
+    priorvector[1]=pbeta(x[1],a0,b0)
+    for (i in 2:200){
+      priorvector[i]=pbeta(x[i],a0,b0)-pbeta(x[i-1],a0,b0)}
+
+    XE=seq(1, length(E), 1)
+    XC=seq(1, length(C), 1)
+    fomega<-rep(0.0,200)
+
+
+    for (j in 1:200){
+      omega=(1/400)+(j-1)*(1/200)
+      komega=(1-omega)/omega
+
+      for (k in 1:samples){
+        Uz<-rep(NA, length(E))
+        XE<-rexp(length(E), rate=komega)
+        XC<-rexp(length(C), rate = 1)
+
+        for (i in 1:length(E)){
+          Uz[i]<-sum(XE[i]>XC)
+        }
+
+        if(sum(Uz) == UE) {fomega[j] = fomega[j]+1} else {}
+      }
+    }
+
+    tot=sum(priorvector*fomega)
+    omegapost=(priorvector*fomega)/tot
+    phiv=rep(0.0,200)
+    for (j in 1:200){
+      phiv[j]=(1/400)+(j-1)*(1/200)}
+    postdis<-data.frame(phiv,omegapost)
+
+    #Folowing finds the mean of the posterior omega distribution
+    #and provides a plot of the distribution.
+    omegabar=sum(phiv*omegapost)
+
+    plot(phiv,omegapost,type="l",xlab="omega_E",ylab="posterior discrete probabilities",main="posterior-solid; prior-dashed")
+    lines(phiv,priorvector,type="l",lty=2)
+
+    cat("mean for omega_E is:"," ","\n")
+    cat(omegabar," ","\n")
+    cat(" ","  ","\n")
+
+    postdis<-data.frame(phiv,omegapost)
+
+    cum_omega=cumsum(omegapost)
+
+    I=1
+    while (cum_omega[I]<(1-prob_interval)/2){
+      I=I+1}
+    qLbelow=phiv[I]-.0025
+
+    if (I!=1){
+      extrap=(1-prob_interval)/2-cum_omega[I-1]
+      probI=cum_omega[I]-cum_omega[I-1]} else {
+        extrap=(1-prob_interval)/2
+        probI=cum_omega[1]}
+    qLv=qLbelow+(.005)*(extrap/probI)
+
+    I=1
+    while (cum_omega[I]<1-(1-prob_interval)/2){
+      I=I+1}
+    qHbelow=phiv[I]-.0025
+    extrapup=1-((1-prob_interval)/2)-cum_omega[I-1]
+    probIu=cum_omega[I]-cum_omega[I-1]
+    qHv=qHbelow+(.005)*(extrapup/probIu)
+    cat(" ","  ","\n")
+    cat("equal-tail area interval"," ","\n")
+    probpercent=100*prob_interval
+    mi=as.character(probpercent)
+    cat(mi,"percent interval limits are:","\n")
+    cat(qLv," ",qHv,"\n")
+    cat(" ","  ","\n")
+
+    omega_E=phiv
+    cumdis<-data.frame(omega_E,cum_omega)
+    #The prH1 is the probability that omega_E is greater than .5.
+
+    prH1=1-cum_omega[round(100)]
+    cum_prior=cumsum(priorvector)
+    priorprH1=1-cum_prior[round(100)]
+    cat("probability that omega_E exceeds .5 is:"," ","\n")
+    cat("prior","  ","posterior","\n")
+    cat(priorprH1,"  ",prH1,"\n")
+    cat(" ","  ","\n")
+
+    #Following finds the Bayes factor for omega_E being greater than .5.
+
+    if ((prH1==1)|(priorprH1==0)){
+      BF10=samples
+      cat("Bayes factor BF10 for omega_E >.5 is estimated to be greater than:"," ","\n")
+      cat(BF10," ","\n")} else {
+        BF10=(prH1*(1-priorprH1))/(priorprH1*(1-prH1))
+        cat("Bayes factor BF10 for omega_E>.5 is:"," ","\n")
+        cat(BF10," ","\n")}
+    #list(posterior_discrete_values=phipost,posterior_cum_distribution=cumdis)
+    return(cat(" ","  ","\n"))} else {}
+
+  if (method=="large"){
+    m1L<-"Following is a beta approximation model for omega_E"
+    m2L<-"when 2*nE*nC/(nE+nC)>19"
+    cat(m1L,m2L,"\n")
+    cat(" ","   ","\n")
+
+    #Following is a Lagrange interpolation method to find posterior a and b
+    #shape parameters for the beta approximation to the omega_E distribution
+    xs=UE/(UE+UC)
+    if (xs>=.5) {x=xs} else {x=1-xs}
+    nH=(2*nE*nC)/(nE+nC)
+    y5=(nH^1.1489)/(.4972+(nH^1.1489))
+    w4=.8-(1/(1+(1.833*nH)))
+    w3=.6-(1/(1+(2.111*nH)))
+    w2=.4-(1/(1+(2.520*nH)))
+    w1=.2-(1/(1+(4.813*nH)))
+    y4=(y5*w4)+(1-w4)*.5
+    y3=(y5*w3)+(1-w3)*.5
+    y2=(y5*w2)+(1-w2)*.5
+    y1=(y5*w1)+(1-w1)*.5
+    Y=c(.5,y1,y2,y3,y4,y5)
+    La0=252-(1627*x)+((12500*x^2)-(15875*x^3)+(10000*x^4)-(2500*x^5))/3
+    La1=-1050+((42775*x)/6)-(38075*.5*x^2)+((75125*x^3)-(48750*x^4)+(12500*x^5))/3
+    La2=1800-(12650*x)+((104800*x^2)-(142250*x^3)+(95000*x^4)-(25000*x^5))/3
+    La3=-1575+(11350*x)+((-96575*x^2)+(134750*x^3)-(92500*x^4)+(25000*x^5))/3
+    La4=700+(14900*x^2)+(15000*x^4)-((15425*x)+(63875*x^3)+(12500*x^5))/3
+    La5=-126+(1879*.5*x)+((-16625*.5*x^2)+(12125*x^3)-(8750*x^4)+(2500*x^5))/3
+    LA=c(La0,La1,La2,La3,La4,La5)
+    ombar=sum(Y*LA)
+    absum=nH*(1.028+(.75*x))+2
+    a=ombar*absum
+    b=(1-ombar)*absum
+    omegabar=ombar
+    if (xs<.5){
+      a=(1-ombar)*absum
+      b=ombar*absum
+      omegabar=1-ombar} else {}
+    na=a-1
+    nb=b-1
+    apost=a0+na
+    bpost=b0+nb
+    cat("The posterior beta shape parameters are:"," ","\n")
+    cat("posterior a"," ","posterior b","\n")
+    cat(apost,"   ",bpost,"\n")
+    cat(" ","  ","\n")
+
+    a=apost
+    b=bpost
+    x=seq(0,1,.005)
+    y=dbeta(x,a,b)
+    y0=dbeta(x,a0,b0)
+    plot(x,y,type="l",xlab="omega_E",ylab="probability density",main="posterior- solid; prior-dashed")
+    lines(x,y0,type="l",lty=2)
+
+    postmean=a/(a+b)
+    postmedian=qbeta(.5,a,b)
+    mc1="posterior mean"
+    mc2="posterior median"
+    cat(mc1,"  ",mc2,"\n")
+    cat(postmean,"    ",postmedian,"\n")
+    cat(" ","  ","\n")
+
+    probpercent=100*prob_interval
+    mi=as.character(probpercent)
+    cat("probability within interval is:"," ","\n")
+    cat(mi,"percent","\n")
+    qlequal=qbeta((1-prob_interval)*.5,a,b)
+    qhequal=qbeta(1-(1-prob_interval)*.5,a,b)
+
+    met1="equal-tail limit values are:"
+    met2=" "
+    cat(met1,met2,"\n")
+    cat(qlequal," ",qhequal,"\n")
+    cat(" ","  ","\n")
+
+    alphaL=seq(0,(1-prob_interval),(1-prob_interval)/1000)
+    qL=qbeta(alphaL,a,b)
+    qH=qbeta(prob_interval+alphaL,a,b)
+    diff=qH-qL
+    I=1
+    mindiff=min(diff)
+    while (diff[I]>mindiff){
+      I=I+1}
+    qLmin=qL[I]
+    qHmax=qH[I]
+    probpercent=100*prob_interval
+    mi=as.character(probpercent)
+    cat(mi,"percent highest-density limits are:","\n")
+    cat(qLmin," ",qHmax,"\n")
+    cat(" ","  ","\n")
+
+    prH1=1-pbeta(.5,a,b)
+    priorprH1=1-pbeta(.5,a0,b0)
+    mH11="probability that omega_E > .5"
+    mH12=" "
+    cat(mH11,mH12,"\n")
+    mH1prior="prior"
+    mH1post="posterior"
+    cat(mH1prior,"  ",mH1post,"\n")
+    cat(priorprH1,"  ",prH1,"\n")
+    cat(" ","  ","\n")
+    if ((prH1==1)|(priorprH1==0)){
+      minf1="Bayes factor BF10 for omega_E >.5 is approaching"
+      minf2="infinity"
+      cat(minf1,minf2,"\n")} else {
+        BF10=(prH1*(1-priorprH1))/(priorprH1*(1-prH1))
+        mBF1="Bayes factor BF10 for omega_E > .5"
+        mBF2="is:"
+        cat(mBF1,mBF2,"\n")
+        cat(BF10," ","\n")}
+
+
+    cat(" ","   ","\n")
+    m1X=" "
+    m2X=" "
+    return(cat(m1X,m2X,"\n"))} else {}
+
+  if ((method!="large")&(method!="small")) {
+    stop("An explicit method stipulation must be either the word large or small.")} else {}
+
+}
